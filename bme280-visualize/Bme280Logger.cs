dd502bd8193aceb609e93678abaa07b9e9da440f
koudenpa <koudenpa@hotmail.com>
2016-06-01 00:58:01 +0900

    長時間動かしたときにI/Oで例外が起きてアプリが落ちていた気がするので、雑にハンドリングした。

--- bme280-visualize/Bme280Logger.cs
+++ bme280-visualize/Bme280Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -29,9 +30,20 @@ namespace bme280_visualize
         public async void Log(Bme280Data data)
         {
             var fileName = "bme280-" + data.Timestamp.ToString("yyyyMMdd") + ".csv";
-            var file = await _folder.CreateFileAsync(
-                Path.Combine(LogFolder, fileName), CreationCollisionOption.OpenIfExists);
-            await FileIO.AppendLinesAsync(file, new string[] { Format(data) });
+            try
+            {
+                // XXX なんでもかんでも非同期にすればいいってものでもないと思う、通常のシーケンシャルな処理にした方がいいか？
+                var file = await _folder.CreateFileAsync(
+                    Path.Combine(LogFolder, fileName), CreationCollisionOption.OpenIfExists);
+                await FileIO.AppendLinesAsync(file, new string[] { Format(data) });
+            }
+            catch (Exception ex)
+            {
+                // XXX I/O に失敗したとおぼわしき時にTMPファイルが残っていたがリカバリ可能なのか？
+                // XXX ログを吐くのが結構面倒くさい
+                // https://code.msdn.microsoft.com/Logging-Sample-for-Windows-ecd3622f
+                Debug.WriteLine(ex);
+            }
         }
 
         public static string Format(Bme280Data data)
